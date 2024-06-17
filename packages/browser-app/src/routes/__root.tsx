ac021f56c8b97fa48dedd4299920441969823f96
yamatatsu <the.kinnikuman@gmail.com>
2024-06-17 22:12:46 +0900

    chore: use router devtools

--- packages/browser-app/src/routes/__root.tsx
+++ packages/browser-app/src/routes/__root.tsx
@@ -1,8 +1,18 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import React from "react";
 
 import GlobalHeader from "@/components/GlobalHeader.tsx";
 import SideNav from "@/components/SideNav.tsx";
+
+// see, https://tanstack.com/router/latest/docs/framework/react/devtools#only-importing-and-using-devtools-in-development
+const TanStackRouterDevtools =
+	process.env.NODE_ENV === "production"
+		? () => null
+		: React.lazy(() =>
+				import("@tanstack/router-devtools").then((res) => ({
+					default: res.TanStackRouterDevtools,
+				})),
+			);
 
 export const Route = createRootRoute({
 	component: () => (
