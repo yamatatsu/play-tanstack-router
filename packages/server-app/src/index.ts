b3549bb7d5252aa668735ac0149d030bd34b6bab
yamatatsu <the.kinnikuman@gmail.com>
2024-07-21 22:32:08 +0900

    feat: plc-data-latest endpoint

--- packages/server-app/src/index.ts
+++ packages/server-app/src/index.ts
@@ -1,13 +1,28 @@
+import { type PlcData, PrismaClient } from "@prisma/client";
 import express from "express";
-import { keyword } from "./module.ts";
-
-console.log({ keyword });
+const prisma = new PrismaClient();
 
 const app = express();
 const port = 3000;
 
 app.get("/", (_req, res) => {
 	res.json({ message: "Hello World!" });
+});
+
+app.get("/plc-data-latest", async (_req, res) => {
+	const plcDataList = await prisma.plcData.findMany({
+		where: { timestamp: { gt: new Date(Date.now() - 10_000) } },
+		orderBy: { timestamp: "desc" },
+	});
+
+	const uniqueList = plcDataList.reduce<PlcData[]>((acc, data) => {
+		if (acc.every((d) => d.dataName !== data.dataName)) {
+			acc.push(data);
+		}
+		return acc;
+	}, []);
+
+	res.json({ plcData: uniqueList });
 });
 
 app.listen(port, () => {
--- packages/server-app/src/module.ts
+++
@@ -1,1 +0,0 @@
-export const keyword = "foo";
